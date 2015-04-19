64a68642854a16ede10afcac5806e79781b38ef8
unknown <mrabonin@gmail.com>
2015-04-19 09:51:36 +0300

    added the jsx

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var App = React.createClass({
+	
+	render: function(){
+		return(
+			<div>
+				
+			</div>
+			)
+
+	}
+});
+
+var SportItem = React.createClass({
+		render: function(){
+			return(
+				<div>
+				
+				</div>
+			)
+		}
+});
+
+React.dender(<App/>,document.body);
